28cec70da4ab604aa94fcbd766aca9237c416ac2
Elijah Pekson <etrpekson1998@gmail.com>
2021-09-23 23:45:33 +0800

    set permissions

--- SharedWallet.sol
+++ SharedWallet.sol
@@ -3,8 +3,18 @@
 pragma solidity ^0.8.5;
 
 contract SharedWallet {
+    address owner;
+    
+    constructor() {
+        owner = msg.sender; // owner is address that deployed smart contract
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not allowed");
+        _;
+    }
 
-    function withdrawMoney(address payable _to, uint _amount) public {
+    function withdrawMoney(address payable _to, uint _amount) public onlyOwner { // only the owner can withdraw
         _to.transfer(_amount);
     }
 
